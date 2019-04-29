9039663e71af69696bbdb7566028718a37e4a729
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-04-29 22:20:28 +0100

    Initial

---
+++ Assets/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public GameObject a, b;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if ((a & b) | (a ^ b))
+                print("(a & b) | (a ^ b)");
+            if (a ^ b)
+                print("a ^ b");
+            if (a | b)
+                print("a | b");
+        }
+    }
+}
